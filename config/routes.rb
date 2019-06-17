f3b70ce9dc849b3c99a8d11312b9e5c4710a005d
Mike Shipilov <git@riseup.net>
2019-06-17 17:01:21 +0200

    Added basic views for teams and the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  # get 'team_roles/index'
-  # get 'skills/index'
-  # get 'teams/index'
-  # get 'teams/new'
-  # get 'teams/show'
-  # get 'teams/edit'
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/edit'
   root to: 'pages#home'
   resources :users, except: [:new, :create, :destroy]
-  resources :teams, only: [:index,:new, :create, :show, :edit]
+  resources :teams
   resources :skills, only: [:index]
   resources :user_skills
   resources :team_roles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
 # Models and controllers generation
