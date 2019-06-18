c7a2b54438c2859406b7fc3aa23790597df254fa
Mike Shipilov <git@riseup.net>
2019-06-18 17:59:53 +0200

    Fixed teams index issues, some other small fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def home
     render layout: "home"
   end
+
+  def index
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -5,10 +5,7 @@ class Team < ApplicationRecord
   has_many :team_roles # to know which team roles are allocated in a team (current members + requests (team roles without assigned user))
   has_many :team_role_skills, through: :team_roles # to know the placeholder skillset ID in a team (real life: users to be found)
   has_many :skills, through: :team_role_skills # to know the placeholder skill names in a team (real life: users to be found)
-
-  has_many :user_team_roles, through: :team_roles # to know the assigned user IDs  in a team (real life: already working users)
-  has_many :users, through: :user_team_roles # to know the assigned users in a team (real life: already working users)
-
+  has_many :users, through: :team_roles # to know the assigned user IDs  in a team (real life: already working users)
   validates :name, presence: true, uniqueness: true
   validates :owner_contact, presence: true
   validates :site_id, presence: true
