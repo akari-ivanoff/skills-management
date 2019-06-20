639f31937e5968882d9507b7dce34faec92bc218
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-20 16:30:47 +0200

    search working everywhere but throws error when less than 3 skills are requested. still to fix by passing full TeamRoleSkill to search service

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,39 +5,13 @@ class PagesController < ApplicationController
   end
 
   def index
-    #query = [params[:query1], params[:query2], params[:query3]].join(" ") # returns a string
     query = params[:query].values
     if query.present?
-    # Count hits per skill queried. Returns an array of Users that match query.
-      queryhits = []
-      query.each do |value|
-        queryhits << User.search_user_by_skill(value)
-      end
-      queryhitsflat = queryhits.flatten
 
-    # create hash containing User object and amount of query hits
-    userhits = {}
-    queryhitsflat.each do |item|
-      key = item
-      userhits[key] = queryhitsflat.count(item)
-    end
-
-    # sorting users on maximum skill hit
-
-    userhits_sorted = userhits.sort.to_h
-    @users = userhits_sorted.keys
+    @users = SortingService.new(query).sort
     else
       @users = User.all
 
-    # TO DO add grouping on skill rating
-
-    # fetch User self-assessment for query hits (or all queries)
-    # userscores_skill1 = []
-    # userhits.keys.each do |hit| #CHANGE TO HASH
-    # UserSkill.where(user_id: hit.id, skill_id: Skill.find_by(name: params[:query][:skill1]).id).first.self_assessment
-    # userscores << hit.user_skills.find_by(user_skill.skill.name: "Javascript").self_assessment
-    # end
-    # create hash containing User ID and stars
   end
 end
 end
