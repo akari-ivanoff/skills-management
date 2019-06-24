88a2a53448e63fec7a7862030136f4c1a0b4cfa3
Mike Shipilov <git@riseup.net>
2019-06-24 09:47:20 +0200

    Fixed main page issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,19 +5,18 @@ class PagesController < ApplicationController
   end
 
   def index
+    if params[:query][:team_role_id].present? # if search is done via placeholder
+      query = TeamRole.find(params[:query][:team_role_id])
+      @queryskills = query.skills.map { |skill| skill.name }
+    else
+      @queryskills = params[:query].values # if search is done via triple search fields
+    end
 
-  if params[:query][:team_role_id].present? # if search is done via placeholder
-    query = TeamRole.find(params[:query][:team_role_id])
-    @queryskills = query.skills.map {|skill| skill.name}
-  else
-    @queryskills = params[:query].values # if search is done via triple search fields
-  end
     if @queryskills.present? # if search is done in any way, fire up the SortingService
-
-    @users = SortingService.new(@queryskills).sort
+      @users = SortingService.new(@queryskills).sort
     else
       @users = User.all # otherwise, show all users
-
-    @team = Team.new # added in order to choose a team, when a match was found on results page (pages/index)
+      @team = Team.new # added in order to choose a team, when a match was found on results page (pages/index)
+    end
   end
 end
