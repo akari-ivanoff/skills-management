b1fa33282e74ba86a34927faf4be0a7e52acfd38
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-24 14:53:19 +0200

    ugly search but working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,16 @@
 import "bootstrap";
+import "select2"
+console.log("hey first console.log");
 
 $(".alert-dismissible").fadeTo(2500, 500).slideUp(500, function(){
   $(".alert-dismissible").alert('close');
 });
+
+
+
+
+$(document).ready(function() {
+  console.log("hey")
+    $('.js-example-basic-multiple').select2();
+});
+
