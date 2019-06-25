122a1be44b0b2624d44008f3cde62a921908c45d
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-25 08:00:59 +0200

    search with improved result ordering, displaying partial matches in gray and sorted user skills per card

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 class Skill < ApplicationRecord
+  include PgSearch
   belongs_to :skill_category
   has_many :user_skills, dependent: :destroy
   has_many :team_role_skills, dependent: :destroy
@@ -7,4 +8,11 @@ class Skill < ApplicationRecord
   has_many :team, through: :team_roles # in order to show all teams that are working with a skill
   validates :name, presence: true, uniqueness: true
   # validates :description
+
+  pg_search_scope :search_by_skillname, against: :name
+
+
+
+
 end
+
--- app/services/sorting_service.rb
+++ app/services/sorting_service.rb
@@ -8,25 +8,33 @@ class SortingService
   def sort
   # Return an array of User objects that match query.
     queryhits = []
+    query_skills = []
     @query.each do |value|
       queryhits << User.search_user_by_skill(value)
+      query_skills << Skill.search_by_skillname(value)
     end
     queryhitsflat = queryhits.flatten
-
-    # create hash containing User object and amount of query hits
+    query_skillsflat = query_skills.flatten
+    # create hash containing User objects and amount of query hits
     userhits = {}
     queryhitsflat.each do |user|
       key = user
       userhits[key] = queryhitsflat.count(user)
     end
 
-    # create hash containing User object and amount of relevant stars
+    # create array containing Skill objects found by PgSearch
+    queryskills_pg = []
+    query_skillsflat.each do |skill|
+      queryskills_pg << skill.name
+    end
+
+    # create hash containing User objects and amount of relevant stars
     userratings = {}
-    counter = 0
     queryhitsflat.each do |user|
+      counter = 0
       key = user
         user.user_skills.each do |userskill|
-          if @query.include? userskill.skill.name
+          if queryskills_pg.include? userskill.skill.name
             counter += userskill.self_assessment
           end
         end
@@ -45,5 +53,6 @@ class SortingService
     resultsarraysorted2 = resultsarraysorted.sort_by { |a| [-a[1]] }
 
     @users = resultsarraysorted2.map {|column| column[0]}
+    array = [@users, queryskills_pg]
     end
 end
